3190e97a47ad8760b85911e446a918e149b0ea7f
David Heinebäck <du222aa@student.lnu.se>
2022-03-22 15:17:52 +0100

    set up cookie options config

---
+++ utils/config.ts
@@ -0,0 +1,10 @@
+export const cookieOptions = {
+  cookieName: process.env.COOKIE_NAME,
+  password: process.env.COOKIE_SECRET,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60,
+    sameSite: 'lax'
+  }
+}
