0fb62cd14a31781d50b4b9c8fd9c4012840fce25
David Heinebäck <du222aa@student.lnu.se>
2022-03-22 17:29:03 +0100

    add: getOAuthTokens to config module

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,7 @@
 import { cookieOptionsConfig } from "./getCookieConfig"
 import getGitLabURI from "./getGitLabURI"
+import { getOAuthTokens } from './getOauthTokens'
 
 export const OauthURI = getGitLabURI()
 export const cookieOptions = cookieOptionsConfig
+export const getOAuthTokensFrom = (code: string) => getOAuthTokens(code)
