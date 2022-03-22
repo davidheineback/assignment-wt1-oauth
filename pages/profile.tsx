db34c28c6fd8e58d9989180852b521472880e54c
David Heinebäck <du222aa@student.lnu.se>
2022-03-22 17:29:32 +0100

    add: fetch of tokens and create session on login

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,28 @@
 import React from 'react'
+import { withIronSessionSsr } from 'iron-session/next'
+import { cookieOptions, getOAuthTokensFrom } from '../utils/config'
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps ({ req, query }: any): Promise<any> {
+    const { code } = query
+
+    const tokenResponse = await getOAuthTokensFrom(code)
+
+    console.log(tokenResponse)
+
+    try {
+        req.session.user = 'David'
+        await req.session.save()
+        return {
+          props: {
+            user: req.session.user
+          }
+        }
+    } catch (error) {
+
+    }
+  }, cookieOptions
+)
 
 function Profile({ user }: any) {
   return (
