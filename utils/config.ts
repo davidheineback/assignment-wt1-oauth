549090229b7596a63460d6429ebd107dd391cb59
David Heinebäck <du222aa@student.lnu.se>
2022-03-22 17:05:31 +0100

    add import from cookieOptionsConfig

--- utils/config.ts
+++ utils/config.ts
@@ -1,14 +1,5 @@
+import { cookieOptionsConfig } from "./getCookieConfig"
 import getGitLabURI from "./getGitLabURI"
 
-export const cookieOptions = {
-  cookieName: process.env.COOKIE_NAME,
-  password: process.env.COOKIE_SECRET,
-  cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60,
-    sameSite: 'lax'
-  }
-}
-
 export const OauthURI = getGitLabURI()
+export const cookieOptions = cookieOptionsConfig
