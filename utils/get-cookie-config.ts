858da0f862e03dcde1261b339f3136a0016bd29e
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 10:59:09 +0100

    refactor: update sameSite Types

---
+++ utils/get-cookie-config.ts
@@ -0,0 +1,22 @@
+import { IronSessionOptions } from 'iron-session'
+
+interface CookieOptions extends IronSessionOptions {
+  cookieName: string,
+  password: string,
+  cookieOptions: {
+    secure: boolean,
+    httpOnly: boolean,
+    maxAge: number
+    sameSite: boolean | 'lax' | 'strict' | 'none' | undefined
+}
+}
+export const cookieOptionsConfig: CookieOptions = {
+  cookieName: process.env.COOKIE_NAME!,
+  password: process.env.COOKIE_SECRET!,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production', // sets the secure param to true in production environment
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60,
+    sameSite: 'lax'
+  }
+}
