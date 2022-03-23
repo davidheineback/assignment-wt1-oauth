bd47cc803031c0d151a9af4adc34b22e6cf084b0
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 10:58:27 +0100

    add: getUserData module

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,9 @@
-import { cookieOptionsConfig } from "./getCookieConfig"
-import getGitLabURI from "./getGitLabURI"
-import { getOAuthTokens } from './getOauthTokens'
+import { cookieOptionsConfig } from "./get-cookie-config"
+import getGitLabURI from "./get-gitlab-uri"
+import { getOAuthTokens } from './get-oauth-tokens'
+import { getGitLabUserFrom } from './get-gitlab-user'
 
 export const OauthURI = getGitLabURI()
 export const cookieOptions = cookieOptionsConfig
 export const getOAuthTokensFrom = (code: string) => getOAuthTokens(code)
+export const getUserData = (access_token: string, token_type:string) => getGitLabUserFrom(access_token, token_type)
