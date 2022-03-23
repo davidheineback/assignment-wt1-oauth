99f20e30919b2f678b1684355e11c89b08e93c43
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 16:10:06 +0100

    add: if to check for session

--- pages/activities.tsx
+++ pages/activities.tsx
@@ -1,53 +1,66 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
+import { Table } from 'react-bootstrap'
 import { withIronSessionSsr } from 'iron-session/next'
-import { cookieOptions, getActivitiesFor } from '../utils/config'
+import { cookieOptions, getActivitiesFor, OAuthURI } from '../utils/config'
 
 export const getServerSideProps = withIronSessionSsr(
-  async function getServerSideProps ({ req, query }: any): Promise<any> {
+  async function getServerSideProps ({ req }: any): Promise<any> {
+    if (!req.session.access_token) {
+      return {
+        redirect: {
+          destination: OAuthURI,
+          permanent: false,
+        }
+      }
+    } else {
 
-    console.log(req.session)
-    const { access_token } = req.session
-    const { id } = req.session.user
-
-    const activities = await getActivitiesFor(id, access_token)
-
-    try {
-        
+      const { access_token } = req.session
+      const { id } = req.session.user
+      
+      const activities = await getActivitiesFor(id, access_token)
+      
+      try {
         return {
           props: {
             activities
           }
         }
-    } catch (error) {
-
+      } catch (error) {
+        
+      }
     }
   }, cookieOptions
   )
-
 
 function Activities({ activities }: any) {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-      <div className={styles.card}>
-        <div>Activities:</div>
-        <ul>
-         {
-           activities.map((activity: any, index: number) => {
-            return (
-                <div key={index} className={styles.card}>
-                  {index}
-                  <ul>{activity.action_name}</ul>
-                  <ul>{activity.created_at}</ul>
-                  <ul>{activity.target_title}</ul>
-                  <ul>{activity.target_type}</ul>
-                  </div>
-            ) 
-           })
-         }
-         </ul>
-      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th scope="col">Action</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Target title</th>
+            <th scope="col">Target type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            activities.map((activity: any, index: number) => {
+              return (
+                <tr key={index}> 
+                  <th scope="row">{activity.action_name}</th>
+                  <td>{activity.created_at}</td>
+                  <td>{activity.target_title}</td>
+                  <td>{activity.target_type}</td>
+                </tr>
+              ) 
+            })
+          }
+        </tbody>
+      </Table>
     </div>
   </main>
   )
