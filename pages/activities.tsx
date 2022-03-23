6882d0cf87e31afcad5b3793852cea93b19b1c23
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 13:07:12 +0100

    add data from session to activities

--- pages/activities.tsx
+++ pages/activities.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import styles from '../styles/Home.module.css'
+import { withIronSessionSsr } from 'iron-session/next'
+import { cookieOptions, getActivitiesFor } from '../utils/config'
+
+export const getServerSideProps = withIronSessionSsr(
+  async function getServerSideProps ({ req, query }: any): Promise<any> {
+
+    console.log(req.session)
+    const { access_token } = req.session
+    const { id } = req.session.user
+
+    const activities = await getActivitiesFor(id, access_token)
+
+    try {
+        
+        return {
+          props: {
+            activities
+          }
+        }
+    } catch (error) {
+
+    }
+  }, cookieOptions
+  )
 
 
-
-function Activities() {
+function Activities({ activities }: any) {
   return (
-    <div>activities</div>
+    <main className={styles.main}>
+      <div className={styles.grid}>
+      <div className={styles.card}>
+        <div>Activities:</div>
+         {
+           activities.map((activity: any, index: number) => {
+            return <div key={index}>{activity.id}</div>
+           })
+         }
+      </div>
+    </div>
+  </main>
   )
 }
 
