f8f0853e5aa9986b8e8025f78d5efdc33c31f463
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 10:58:11 +0100

    add: image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['gitlab.lnu.se', '*gravatar.com']
+  }
 }
-
-module.exports = nextConfig
