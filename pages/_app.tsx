59609106aed4cbdb52a008c0d4166756a6201520
David Heinebäck <du222aa@student.lnu.se>
2022-03-23 16:09:15 +0100

    add Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href='/'>GitLab OAuth Application</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/activities">Activities</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
